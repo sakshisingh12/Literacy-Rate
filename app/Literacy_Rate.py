6817c5a34e5eca250ba8e529dbed7156daac811a
Sakshi Singh Dangi <78265680+sakshisingh12@users.noreply.github.com>
2021-12-09 22:11:35 +0530

    Update Literacy_Rate.py

--- app/Literacy_Rate.py
+++ app/Literacy_Rate.py
@@ -2,7 +2,7 @@ from typing import List
 import streamlit as st
 import numpy as np
 import pandas as pd
-import plotly as px
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 st.title("Literacy Rate")
